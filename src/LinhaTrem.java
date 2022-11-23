a54836824b96636407d93fabfe1d0647cef48ae3
Pedro <pedrobiqua@gmail.com>
2022-11-22 22:45:01 -0300

    Adicionando comentário

--- src/LinhaTrem.java
+++ src/LinhaTrem.java
@@ -1,14 +1,26 @@
 public class LinhaTrem {
     /**
-     * True - Ponto de parada
+     * Ponto de junção
      */
     public static boolean[] pontoDeJuncao = new boolean[4];
 
+    /**
+     * Posições dos trens no pontos A
+     */
     public static boolean[] posicaoLinhaA = new boolean[4];
 
+    /**
+     * Posições dos trens no pontos B
+     */
     public static boolean[] posicaoLinhaB = new boolean[4];
 
+    /**
+     * Posição do trem 1
+     */
     public static int posicaoTrem1 = 0;
 
+    /**
+     * Posição do trem 2
+     */
     public static int posicaoTrem2 = 0;
 }
