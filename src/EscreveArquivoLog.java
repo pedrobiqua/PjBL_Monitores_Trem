e3b61015a8ae2e765b560bfc00ff299c36a23b34
Pedro <pedrobiqua@gmail.com>
2022-11-22 23:17:15 -0300

    Melhorias no EscreveArquivoLog.java

--- src/EscreveArquivoLog.java
+++ src/EscreveArquivoLog.java
@@ -1,10 +1,16 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
 
 public class EscreveArquivoLog {
     
+    public static boolean fileAExists = false;
+    public static boolean fileBExists = false;
+
     /**
      * Escreve no arquivo de log
      * @param nomeArquivo
@@ -12,14 +18,61 @@ public class EscreveArquivoLog {
      */
     public void escreverArquivoTxt(String nomeArquivo, ArrayList<String> texto) {
         try {
-            FileWriter arquivo = new FileWriter(nomeArquivo, true);
-            PrintWriter gravarArquivo = new PrintWriter(arquivo);
-            for (String linha : texto) {
-                gravarArquivo.println(linha);
+            File file = atualizaArquivo(nomeArquivo);
+            if (file != null) {
+                FileWriter arquivo = new FileWriter(file, true);
+                PrintWriter gravarArquivo = new PrintWriter(arquivo);
+                for (String linha : texto) {
+                    gravarArquivo.println(linha);
+                }
+                arquivo.close();
             }
-            arquivo.close();
         } catch (IOException e) {
             System.out.println("Erro ao escrever no arquivo: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Atualiza os arquivos de log
+     */
+    public File atualizaArquivo(String nomeArquivo) {
+        try {
+            Path path = FileSystems.getDefault().getPath("");
+            String directoryName = path.toAbsolutePath().toString();
+            File file = new File(directoryName + "\\" + nomeArquivo);
+
+            if (!file.exists()) {
+                if (nomeArquivo.equals("TremA.txt")) {
+                    fileAExists = true;
+                } else if (nomeArquivo.equals("TremB.txt")) {
+                    fileBExists = true;
+                } else {
+                    System.out.println("Arquivo não encontrado, esse arquivo não bate com TremA.txt e TremB.txt, nome arquivo : " + nomeArquivo);
+                    return null;
+                }
+                file.createNewFile();
+            } else {
+                if (nomeArquivo.equals("TremA.txt")) {
+                    if (!fileAExists) {
+                        file.delete();
+                        file.createNewFile();
+                        fileAExists = true;
+                    }
+                } else if (nomeArquivo.equals("TremB.txt")) {
+                    if (!fileBExists) {
+                        file.delete();
+                        file.createNewFile();
+                        fileBExists = true;
+                    }
+                } else {
+                    System.out.println("Arquivo não encontrado, esse arquivo não bate com TremA.txt e TremB.txt, nome arquivo : " + nomeArquivo);
+                    return null;
+                }
+            }
+            return file;
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizaro arquivo " + nomeArquivo + " : " + e.getMessage());
+            return null;
         }
     }
 }
